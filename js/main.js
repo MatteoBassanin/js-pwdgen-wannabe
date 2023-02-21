52aa9ce9bf2dc25eebaac0da9d08b1280b97e7a1
MatteoBassanin <matteo.bassanin@gmail.com>
2023-02-21 15:43:43 +0100

    done_added_name_surname_color

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 const yourName = prompt("Come ti chiami?");
 const yourSurname = prompt("Qual'è il tuo cognome ?");
-const yourFavColor
-document.getElementById("password_sicurissima").innerHTML = "la tua passaword sicurissima è" + yourName;
+const yourFavColor = prompt("Qual'è il tuo colore preferito?");
+
+const passwordSicurissima = yourName + yourSurname + yourFavColor + "23";
+
+document.getElementById("password_sicurissima").innerHTML = "la tua passaword sicurissima è " + passwordSicurissima ;
